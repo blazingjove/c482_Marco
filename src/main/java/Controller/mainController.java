619c1f72e0a9edf444decbfa80a48797435d0c73
blazingjove <blazingjove@gmail.com>
2024-04-27 14:40:09 -0500

    - added more features to add product view

--- src/main/java/Controller/mainController.java
+++ src/main/java/Controller/mainController.java
@@ -3,17 +3,13 @@ package Controller;
 import Model.Inventory;
 import Model.Part;
 import Model.Product;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -36,16 +32,13 @@ public class mainController implements Initializable{
     @FXML
     private AnchorPane mainPane;
     Stage stage;
-    public void onExitClicked(ActionEvent actionEvent) {
+    public void onExitClicked() {
         stage = (Stage) mainPane.getScene().getWindow();
         System.out.println("Program closed");
         stage.close();
     }
 
     //variables for parts
-    //public Button onPartModify;
-    //public Button partDeleteButton;
-    //public Button onPartAdd;
 
     @FXML
     private TableView<Part> partTable;
@@ -57,11 +50,6 @@ public class mainController implements Initializable{
     private TableColumn<Part, Integer>  partInventory;
     @FXML
     private TableColumn<Part, Integer>  partCost;
-
-    //variable for products
-    //public Button onProductModify;
-    //public Button productDeleteButton;
-
     @FXML
     private TableView<Product> productTable;
     @FXML
@@ -72,15 +60,14 @@ public class mainController implements Initializable{
     private TableColumn<Product, Integer>  productInventory;
     @FXML
     private TableColumn<Product, Integer>  productCost;
-    public Button onProductAdd;
 
     //add Part button opens add part window
     @FXML
-    public void onPartAdd(ActionEvent actionEvent) {
+    public void onPartAdd() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource("/c482/views/addPartView.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Scene scene = new Scene(fxmlLoader.load());
             Stage stage = new Stage();
             stage.setScene(scene);
 
@@ -105,7 +92,7 @@ public class mainController implements Initializable{
 
     // modify part button opens modify part window and closes main temp
     @FXML
-    public void onPartModify(ActionEvent actionEvent) {
+    public void onPartModify() {
         Part selectedPart = partTable.getSelectionModel().getSelectedItem();
         if (selectedPart != null) {
             try {
@@ -133,15 +120,15 @@ public class mainController implements Initializable{
 
     // onPartDelete button set to delete data selected
     @FXML
-    public void onPartDelete(ActionEvent actionEvent){
+    public void onPartDelete(){
         Part selectedPart = partTable.getSelectionModel().getSelectedItem();
         Inventory.deletePart(selectedPart);
     }
 
     // onProductDelete button will delete selected product
     @FXML
-    public void onProductDelete(ActionEvent actionEvent){
-        System.out.println("test1");
+    public void onProductDelete(){
+        System.out.println("test 1");
         Product selectedProduct = productTable.getSelectionModel().getSelectedItem();
         Inventory.deleteProduct(selectedProduct);
     }
@@ -149,11 +136,11 @@ public class mainController implements Initializable{
 
     //opens product add view and closes main temporarily
     @FXML
-    public void onProductAdd(ActionEvent actionEvent){
+    public void onProductAdd(){
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource("/c482/views/addProductView.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Scene scene = new Scene(fxmlLoader.load());
             Stage stage = new Stage();
             stage.setScene(scene);
 
@@ -172,11 +159,11 @@ public class mainController implements Initializable{
     }
 
         @FXML
-        public void onProductModify(ActionEvent actionEvent){
+        public void onProductModify(){
             try {
                 FXMLLoader fxmlLoader = new FXMLLoader();
                 fxmlLoader.setLocation(getClass().getResource("/c482/views/modifyProductView.fxml"));
-                Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+                Scene scene = new Scene(fxmlLoader.load());
                 Stage stage = new Stage();
                 stage.setScene(scene);
 
@@ -225,19 +212,17 @@ public class mainController implements Initializable{
         FilteredList<Part> filteredPartList = new FilteredList<>(Inventory.getAllParts(), p -> true);
 
         // Bind the filtered list to the partSearchTextField text property
-        partSearchTextField.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredPartList.setPredicate(part -> {
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
+        partSearchTextField.textProperty().addListener((observable, oldValue, newValue) -> filteredPartList.setPredicate(part -> {
+            if (newValue == null || newValue.isEmpty()) {
+                return true;
+            }
 
-                String lowerCaseFilter = newValue.toLowerCase();
+            String lowerCaseFilter = newValue.toLowerCase();
 
-                // Compare all part attributes with the search text
-                return part.getName().toLowerCase().contains(lowerCaseFilter)
-                        || String.valueOf(part.getId()).contains(lowerCaseFilter);
-            });
-        });
+            // Compare all part attributes with the search text
+            return part.getName().toLowerCase().contains(lowerCaseFilter)
+                    || String.valueOf(part.getId()).contains(lowerCaseFilter);
+        }));
 
         // Create a SortedList to display the filtered items in the table
         SortedList<Part> sortedPartList = new SortedList<>(filteredPartList);
@@ -246,27 +231,22 @@ public class mainController implements Initializable{
         // Set the sorted list as the items of the partTable
         partTable.setItems(sortedPartList);
 
-        /**
-         * same as  above to filter product text area input in product table
-         */
+/*
+ same as  above to filter product text area input in product table
+*/
 
         // Create a FilteredList and SortedList for the ProductTable
         FilteredList<Product> filteredProductList = new FilteredList<>(Inventory.getAllProducts(), p -> true);
 
 // Bind the filtered list to the productSearchTextField text property
-        productSearchTextField.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredProductList.setPredicate(product -> {
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-
-                String lowerCaseFilter = newValue.toLowerCase();
-
-                // Compare all Product attributes with the search text
-                return product.getName().toLowerCase().contains(lowerCaseFilter)
-                        || String.valueOf(product.getId()).contains(lowerCaseFilter);
-            });
-        });
+        productSearchTextField.textProperty().addListener((observable, oldValue, newValue) -> filteredProductList.setPredicate(product -> {
+            if (newValue == null || newValue.isEmpty()) {
+                return true;
+            }
+            String lowerCaseFilter = newValue.toLowerCase();
+            return product.getName().toLowerCase().contains(lowerCaseFilter)
+                    || String.valueOf(product.getId()).contains(lowerCaseFilter);
+        }));
 
 // Create a SortedList to display the filtered items in the table
         SortedList<Product> sortedProductList = new SortedList<>(filteredProductList);
